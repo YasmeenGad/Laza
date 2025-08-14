e8acf0dd3c0cf98a70e5c27f42bd65aac39eba2b
YasmeenGad <gadyasmeen335@gmail.com>
2025-08-15 00:58:18 +0300

    chore: add flutter localization configuration

--- lib/laza.dart
+++ lib/laza.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:laza/core/config/media_query_config.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/routes/route_names.dart';
 import 'core/styles/app_colors.dart';
+import 'generated/l10n.dart';
 
 class Laza extends StatelessWidget {
   const Laza({super.key});
@@ -17,6 +19,14 @@ class Laza extends StatelessWidget {
             kIsWeb ? Size(context.width, context.height) : const Size(375, 812),
         minTextAdapt: true,
         child: MaterialApp(
+            locale: Locale('ar'),
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
             debugShowCheckedModeBanner: false,
             builder: (context, child) {
               return Scaffold(
