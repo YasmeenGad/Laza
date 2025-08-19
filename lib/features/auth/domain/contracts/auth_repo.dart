94a0d0edb64ac089cbaf8b187e7dfbe770752d90
YasmeenGad <gadyasmeen335@gmail.com>
2025-08-19 23:24:36 +0300

    feat(auth): create sign in with google method

--- lib/features/auth/domain/contracts/auth_repo.dart
+++ lib/features/auth/domain/contracts/auth_repo.dart
@@ -1,1 +1,6 @@
-abstract class AuthRepo {}
+import 'package:laza/core/errors/result.dart';
+import 'package:laza/features/auth/domain/entities/google_user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Result<GoogleUserEntity>> signInWithGoogle();
+}
